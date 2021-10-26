b1a4d2843bb6488d570806b159ecc699c1a0d4b8
rezkon <rezk@example.com>
2021-10-26 16:40:35 +1100

    Frida

---
+++ frida/iOS/get_realmkey.js
@@ -0,0 +1,30 @@
+Interceptor.attach(ObjC.classes.RLMRealmConfiguration['- setEncryptionKey:'].implementation, {
+    onEnter: function(args){
+        console.log("Called!")
+        console.log(args[2])
+        var key = new ObjC.Object(ptr(args[2]))
+        
+        //log(key.toString())
+       // console.log(JSON.stringify(key.bytes().toString(), null, 4));
+
+
+        console.log("Key: ")
+        console.log(hexdump(ptr(key.bytes()), {
+            length: 64,
+            header: true,
+            ansi: true
+        }))
+        
+
+     
+        
+  
+        
+    }, 
+    onLeave: function(args){
+        var key = args[2]
+       
+
+    }
+});
+
