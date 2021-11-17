69ce94400548942b031f7dd8819307e6544eeaac
Luke <luke>
2021-11-17 22:59:51 +1100

    Adding more

--- iOS/ios_strong.py
+++ iOS/ios_strong.py
@@ -3,10 +3,101 @@ import pathlib
 import re
 
 from strongarm.cli.utils import disassemble_method
-from strongarm.macho import MachoParser, MachoAnalyzer
+from strongarm.macho import MachoParser, MachoAnalyzer, VirtualMemoryPointer
+from strongarm.objc import *
 
+#Todo: Implement Denises bugs in this.
+#
 
 # Provide a directory..
+#for objc_cls in analyzer.objc_classes():
+ #   for objc_sel in objc_cls.selectors:
+  #      list_search = re.search('listener:([a-zA-Z0-9+/]+)', objc_sel.name)
+   #     if list_search:
+    #        clean = list_search.group(1)
+     #       print("FOUND XPC_Should_Accept:, File: {}\n".format(macho_bin))
+      #      # dis_str = disassemble_method(binary, objc_sel.name)
+       #     print(f'\tmethod: {objc_sel.name} @ {hex(objc_sel.implementation)}')
+        #    for x in analyzer.get_objc_methods():
+         #       if x.objc_sel.name == "listener:shouldAcceptNewConnection:":
+          #          print("Found: {}".format(x.objc_sel.name))
+           #         dis = disassemble_method(binary, x)
+            #        print(dis)
+#for x in analyzer.get_objc_methods()
+
+
+
+def function_use(macho_bin):
+    analyzer = MachoAnalyzer.get_analyzer(binary)
+
+
+
+
+def xpc_get_string_basic(macho_bin):
+    sel_name = []
+    try:
+        parser = MachoParser(pathlib.Path(macho_bin))
+        binary = parser.get_arm64_slice();
+        analyzer = MachoAnalyzer.get_analyzer(binary)
+        get_string_symbol = f'_xpc_dictionary_get_string'
+        xpc_symbol = analyzer.callable_symbol_for_symbol_name(get_string_symbol)
+        for xref in analyzer.calls_to(xpc_symbol.address):
+            print('Found call to {} at {}'.format(xpc_symbol, xref.caller_addr))
+
+            function_anaylzer = ObjcFunctionAnalyzer.get_function_analyzer(binary, xref.caller_func_start_address)
+            call_instr = ObjcInstruction.parse_instruction(
+                function_anaylzer,
+                function_anaylzer.get_instruction_at_address(xref.caller_addr)
+            )
+            print(call_instr)
+
+
+
+    except Exception as macho_err:
+        print(macho_err)
+
+
+
+
+def xpc_get_string(macho_bin):
+    sel_name = []
+    try:
+        parser = MachoParser(pathlib.Path(macho_bin))
+        binary = parser.get_arm64_slice();
+        analyzer = MachoAnalyzer.get_analyzer(binary)
+      # test = analyzer.string_xrefs_to('command')
+      # print(test)
+
+        imported_symbols = analyzer.imported_symbols_to_symbol_names
+        for symbol_ptr, symbol_name in imported_symbols.items():
+            mach_strings = re.search('xpc.dictionary_get_string*\w+', symbol_name)
+            if mach_strings:
+                print("{}:xpc.dictionary_get_string".format(symbol_ptr))
+                get_string_calls = analyzer.callable_symbol_for_address(symbol_ptr)
+                print(get_string_calls)
+
+
+
+
+    except Exception as macho_err:
+        print(macho_err)
+
+
+
+#def xpc_get_string(macho_bin):
+#    sel_name = []
+#    try:
+#        parser = MachoParser(pathlib.Path(macho_bin))
+#        binary = parser.get_arm64_slice();
+#        analyzer = MachoAnalyzer.get_analyzer(binary)
+#        for imp_sym in analyzer.imported_symbols:
+#            mach_strings = re.search('xpc.dictionary_get_string*\w+', imp_sym)
+#            if mach_strings:
+#               print("Found XPC Function: {}".format(mach_strings.group(0)))
+#                print("Import Symbols: {}".format(analyzer.imported_symbols_to_symbol_names))
+
+
+
 
 
 def xpc_construct(macho_bin):
@@ -21,6 +112,7 @@ def xpc_construct(macho_bin):
                 print("Found XPC Function: {}".format(mach_strings.group(0)))
     except Exception as macho_err:
         print(macho_err)
+
 
 
 def xpc_should_accept(macho_bin):
@@ -42,6 +134,9 @@ def xpc_should_accept(macho_bin):
                             print("Found: {}".format(x.objc_sel.name))
                             dis = disassemble_method(binary, x)
                             print(dis)
+                        if x.objc_sel.name == 'xpc_connection_create_mach_service':
+                            print("Mach: {}".format(x.objc_sel.name))
+
                     #disassemble_function(binary, hex(objc_sel.name))
                     for objc_ivar in objc_cls.ivars:
                         print(f'\tivar: {objc_ivar.name}{objc_ivar.class_name}')
@@ -65,8 +160,8 @@ def xpc_should_accept(macho_bin):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pathlist = pathlib.Path('').glob('*')
+    pathlist = pathlib.Path('/Users/lsymons/XPC/libexec/').glob('*')
     for file in pathlist:
         print("Parsing: {}\n".format(file))
-        xpc_construct(file)
-        xpc_should_accept(file)
+        xpc_get_string_basic(file)
+
